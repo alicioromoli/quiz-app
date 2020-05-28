f9dd4618f04b945a96942f12c164512143381a23
Alicio Romoli <alicioromoli@Alicios-iMac.local>
2020-05-28 19:07:25 +0100

    initialize git for first commit

---
+++ js/app.js
@@ -0,0 +1,147 @@
+import Question from './Question.js'
+import Quiz from './Quiz.js'
+
+const App = (() => {
+    // cache the DOM
+    const quizEl = document.querySelector('.javQuiz')
+    const quizQuestionEl = document.querySelector('.javQuiz__question')
+    const trackerEl = document.querySelector('.javQuiz__tracker')
+    const taglineEl = document.querySelector('.javQuiz__tagline')
+    const choicesEl = document.querySelector('.javQuiz__choices')
+    const progressInnerEl = document.querySelector('.progress__inner')
+    const nextButtonEl = document.querySelector('.next')
+    const restartButtonEll = document.querySelector('.restart')
+
+    const q1 = new Question(
+        "What was the first president of US",
+        ["Barrack","Osama","George","Monkey"],
+        2
+    )
+    const q2 = new Question(
+        "What was JavaScript created",
+        ["June 1995","May 1999","July 1885","September 1996"],
+        1
+    )
+    const q3 = new Question(
+        "What does the CSS stand for",
+        ["County sherif service","Cascading sexy sheets","Cascading style sheet", "Monkey"],
+        2
+    )
+    const q4 = new Question(
+        "The full form of HTML is",
+        ["Hyper Text Markup Language","Hold the mic","Whaaaaaat","ERROR"],
+        1
+    )
+    const q5 = new Question(
+        "console.log(typeof []) would return what?",
+        ["Object","Array","NULL","array"],
+        0
+    )
+
+    const quiz = new Quiz([q1,q2,q3,q4,q5])
+
+    const listeners = _ => {
+        nextButtonEl.addEventListener('click', function() {
+            const selectedRadioElem = document.querySelector('input[name="choice"]:checked')
+            if (selectedRadioElem){
+                const key = Number(selectedRadioElem.getAttribute('data-order'))
+                quiz.guess(key)
+                renderAll()
+            }
+        })
+
+        restartButtonEll.addEventListener('click', function () {
+            //1. reset the quiz
+            quiz.reset()
+            // 2. renderAll
+            renderAll()
+            // 3. restore the next button
+            nextButtonEl.style.opacity = 1
+        })
+    }
+
+    const setValue = (elem, value) => {
+        elem.innerHTML = value
+    }
+
+    const renderQuestion = _ => {
+        const question = quiz.getCurrentQuestion().question
+        setValue(quizQuestionEl, question)
+    }
+
+    const renderChoicesElements = _ => {
+        let markup = ""
+        const currentChoices = quiz.getCurrentQuestion().choices
+        currentChoices.forEach((elem,index) => {
+            markup += `
+                <li class="javQuiz__choice">
+                    <input type="radio" name="choice" class="javQuiz__input" data-order= "${index}" id="choice${index}">
+                    <label for="choice${index}" class="javQuiz__label">
+                        <i></i>
+                        <span>${elem}</span
+                    </label>
+                </li>
+            `
+        })
+        setValue(choicesEl, markup)
+    }
+
+    const renderTracker = _ => {
+        const index = quiz.currentIndex
+        setValue(trackerEl, `${index + 1} of ${quiz.questions.length}`)
+    }
+
+    const getPercentage = (num1, num2) => {
+        return Math.round((num1/ num2) * 100)
+    }
+
+    const launch = (width, maxPercent) => {
+        let loadingBar = setInterval(() => {
+            if(width > maxPercent){
+                clearInterval(loadingBar)
+            } else {
+                width++
+                progressInnerEl.style.width = `${width}%`
+            }
+        }, 3)
+    }
+
+    const renderProgress = _ => {
+        // 1. width
+        const currentWidth = getPercentage(quiz.currentIndex, quiz.questions.length)
+        // 2. launch(0, width)
+        launch(0, currentWidth)        
+    }
+
+    const renderEndScreen = _ => {
+        setValue(quizQuestionEl, `Great Job!`)
+        setValue(taglineEl, `Complete!`)
+        setValue(trackerEl, `Your Score is ${getPercentage(quiz.score, quiz.questions.length)}%`)
+        nextButtonEl.style.opacity = 0
+        renderProgress()
+    }
+
+    const renderAll = _ =>{
+        if(quiz.hasEnded()) {
+            // renderEndScreen
+            renderEndScreen()
+        } else {
+            // 1. render the question
+            renderQuestion()
+            // 2. Render the choices elements
+            renderChoicesElements()
+            // 3. render tracker
+            renderTracker()
+            // 4. render the progress
+            renderProgress()
+        }
+    }
+
+    return {
+        renderAll: renderAll,
+        listeners: listeners
+    }
+})()
+
+App.renderAll()
+App.listeners()
